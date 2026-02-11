9e52deadfa62a39f47f2923de7ec8e17d29327e2
3r1cZ <114354109+3r1cZ@users.noreply.github.com>
2026-02-11 18:46:13 -0500

    Complete step 1 - preprocessing

---
+++ preprocessing.py
@@ -0,0 +1,68 @@
+import json
+import re
+import string
+from nltk.stem import PorterStemmer
+
+# get stopwords from provided html file
+with open("stopwords.html", "r", encoding="utf-8") as f:
+    html_content = f.read()
+
+# extract content inside <pre>...</pre>
+match = re.search(r"<pre>(.*?)</pre>", html_content, re.DOTALL | re.IGNORECASE)
+if match:
+    pre_content = match.group(1)
+else:
+    pre_content = ""
+
+# Split into words, strip whitespace, ignore empty lines
+stopwords = [word.strip() for word in pre_content.splitlines() if word.strip()]
+
+stemmer = PorterStemmer()
+
+def preprocess_text(text):
+
+    # 1. lowercase
+    text = text.lower()
+
+    # remove html
+    text = re.sub(r'<.*?>', '', text)
+
+    # remove punctuation
+    text = text.translate(str.maketrans('', '', string.punctuation))
+
+    # remove numbers
+    text = re.sub(r'\d+', '', text)
+
+    # tokenize (split on whitespace)
+    tokens = text.split()
+
+    # remove stopwords
+    tokens = [token for token in tokens if token not in stopwords]
+
+    # stemming
+    tokens = [stemmer.stem(word) for word in tokens]
+
+    return tokens
+
+
+def preprocess_document(document):
+
+    documents_tokens = {}
+
+    with open(document, 'r', encoding='utf-8') as f:
+        for line in f:
+            doc = json.loads(line)
+
+            doc_id = doc["_id"]
+
+            # combine title + abstract text
+            text = doc.get("title", "") + " " + doc.get("text", "")
+
+            tokens = preprocess_text(text)
+
+            documents_tokens[doc_id] = tokens
+
+    return documents_tokens
+
+tokens = preprocess_document("corpus.jsonl")
+print("TOKENS:\n", tokens)
